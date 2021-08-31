cd03072596ca103d51acefaad897f1f561075814
BuffaloSniper123 <89615058+BuffaloSniper123@users.noreply.github.com>
2021-08-31 16:08:32 -0700

    Add files via upload

---
+++ sketchTest.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(400,400);
+    background("black");
+    console.log("background done")
+  }
+  function draw(){
+    textSize(20)
+    fill("white")
+    text("hi",100,100)
+    console.log("text done")
+    console.log("where is my console.log? POOOP")
+  }
